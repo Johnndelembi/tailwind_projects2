c5273ed37f41823a30f4420b9a39911f3e138beb
John <williamjohnie61@gmail.com>
2024-08-10 18:13:43 +0300

    First Commit - this is the first commit of this webpage

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./build/*.html'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '14440px',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
